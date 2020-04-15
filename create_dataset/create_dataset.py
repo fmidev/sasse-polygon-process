401e64263e09e25e0455a969f77c0cf9d12facc9
Roope Tervo <roope.tervo@fmi.fi>
2020-04-15 09:23:24 +0300

    added support for energiateollisuus

--- create_dataset/create_dataset.py
+++ create_dataset/create_dataset.py
@@ -29,10 +29,13 @@ from sqlalchemy import create_engine
 import pandas.io.sql as sqlio
 
 from dask.distributed import Client, progress
+from dask.diagnostics import ProgressBar
 import dask.dataframe as dd
 import dask
 from dask import delayed
 import dask.array as dask_array
+
+from tqdm import tqdm
 
 from config import read_options
 
@@ -133,6 +136,71 @@ def create_dataset_loiste_jse(db_params, start, end, meta_params, geom_params, s
 
     return df
 
+def create_dataset_energiateollisuus(db_params, start, end, meta_params, geom_params, storm_params, outage_params, transformer_params, all_params):
+    """ Gather dataset from db """
+    #print('Reading data for {}-{}...'.format(start, end))
+
+    conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (db_params['database'], db_params['user'], db_params['host'], db_params['password']))
+
+    sql = """
+        SELECT
+        """
+    first = True
+    for p in meta_params:
+        if not first:
+            sql += ','
+        first = False
+        sql += "a.{}".format(p)
+
+    for p in geom_params:
+        sql += ',ST_AsText(a.{}) as {}'.format(p, p)
+
+    for p in storm_params:
+            sql += ',"{}"'.format(p)
+    for p in outage_params:
+        sql += ',c.{}'.format(p)
+
+    sql += """
+        FROM
+        sasse.stormcell a
+        INNER JOIN sasse.stormcell_features b ON a.id = b.polygon_id
+        LEFT JOIN (
+            SELECT
+                b.id,
+                SUM(transformers) AS outages,
+                SUM(clients) AS customers
+        FROM
+            sasse.ene_outages aa,
+            sasse.stormcell b,
+            sasse.regions c
+        WHERE
+            date_trunc('hour', aa.start AT TIME ZONE 'Europe/Helsinki' AT TIME ZONE 'UTC') + interval '1 hour' = point_in_time
+            AND ST_Intersects(st_setsrid (b.geom, 4326), st_setsrid (c.geom, 4326))
+            AND aa.area = c.aluetunnus
+            AND b.point_in_time > '{start}'
+            AND b.point_in_time <= '{end}'
+        GROUP BY
+            b.id) c ON c.id = a.id
+        WHERE
+        st_intersects(ST_MakeEnvelope(20.6, 59.8, 31.5, 70.2, 4326), st_setsrid (a.geom, 4326))
+        AND point_in_time > '{start}'
+        AND point_in_time <= '{end}'
+    """.format(start=start, end=end)
+
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    results = cursor.fetchall()
+
+    df = pd.DataFrame(results, columns=all_params)
+
+    df = sqlio.read_sql_query(sql, conn)
+    df.loc[:, 'geom'] = df.loc[:, 'geom'].apply(wkt.loads)
+
+    #df = gpd.GeoDataFrame(df, geometry='geom')
+
+    return df
+
+
 def save_dataset(df, db_params, table_name='classification_dataset'):
     """
     Save classification dataset into the db
@@ -198,11 +266,12 @@ def main():
     transformers_params = ['transformers', 'all_customers']
 
     storm_params = polygon_params + met_params
+
     all_params = meta_params + geom_params + storm_params + outage_params
 
     # Read data from database
     starttime = datetime.datetime.strptime('2010-01-01', "%Y-%m-%d")
-    endtime = datetime.datetime.strptime('2019-01-01', "%Y-%m-%d")
+    endtime = datetime.datetime.strptime('2010-01-03', "%Y-%m-%d")
 
     logging.info('Reading data for {}-{}'.format(starttime, endtime))
 
@@ -212,12 +281,15 @@ def main():
 
         end = start + timedelta(days=1)
 
-        dfs.append(delayed(create_dataset_loiste_jse)(db_params, start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d'), meta_params, geom_params, storm_params, outage_params, transformers_params, all_params))
+        if options.dataset == 'loiste_jse':
+            dfs.append(delayed(create_dataset_loiste_jse)(db_params, start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d'), meta_params, geom_params, storm_params, outage_params, transformers_params, all_params))
+        else:
+            dfs.append(delayed(create_dataset_energiateollisuus)(db_params, start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d'), meta_params, geom_params, storm_params, outage_params, transformers_params, all_params))
 
         start = end
 
-    df = dask.compute(*dfs)
-    progress(df)
+    with ProgressBar():
+        df = dask.compute(*dfs)
 
     dataset = pd.concat(df)
 
@@ -228,10 +300,13 @@ def main():
     #g_dataset.sort_values(by=['point_in_time'], inplace=True)
     logging.info('\nDone. Found {} records'.format(g_dataset.shape[0]))
 
-    # Drop storm objects without customers or transformers, they are outside the range
+    #
     g_dataset.loc[:,['outages','customers']] = g_dataset.loc[:,['outages','customers']].fillna(0)
     g_dataset.loc[:,['outages','customers']] = g_dataset.loc[:,['outages','customers']].astype(int)
-    g_dataset.dropna(axis=0, subset=['all_customers', 'transformers'], inplace=True)
+
+    # Drop storm objects without customers or transformers, they are outside the range
+    if options.dataset == 'loiste_jse':
+        g_dataset.dropna(axis=0, subset=['all_customers', 'transformers'], inplace=True)
 
     # Drop rows with missing meteorological params
     for p in met_params:
@@ -250,7 +325,7 @@ def main():
         ('Forest canopy cover', 's3://fmi-asi-data-puusto/luke/2017/latvusto/puusto_latvusto_suomi_4326.tif'),
         ('Forest site main class', 's3://fmi-asi-data-puusto/luke/2017/paatyyppi/puusto_paatyyppi_suomi_4326.tif')
         ]
-    #paths = [('Forest canopy cover', 's3://fmi-asi-data-puusto/luke/2017/latvusto/puusto_latvusto_suomi_4326.tif')]
+    paths = [('Forest canopy cover', 's3://fmi-asi-data-puusto/luke/2017/latvusto/puusto_latvusto_suomi_4326.tif')]
     chunks = {'y': 5000, 'x': 5000}
 
     def stats(row, data=None, filename=None):
@@ -266,11 +341,18 @@ def main():
         #     else:
         #         return np.percentile(arr, axis=axis, q=q)
 
+        def x_slice(bounds, ar):
+            return slice(max(bounds[0], min(ar.x).values), min(bounds[2], max(ar.x).values))
+        def y_slice(bounds, ar):
+            return slice(max(bounds[3], min(ar.y).values), min(bounds[1], max(ar.y).values))
+
         bounds = row.geom.bounds
         print(".", end="")
 
         data = xr.open_rasterio(filename, chunks=chunks)
-        data_bbox = data.sel(x=slice(bounds[0],bounds[2]), y=slice(bounds[3], bounds[1]))
+
+        #data_bbox = data.sel(x=slice(bounds[0],bounds[2]), y=slice(bounds[3], bounds[1])
+        data_bbox = data.sel(x=x_slice(bounds, data), y=y_slice(bounds, data))
         data_bbox = data_bbox.where(data_bbox < 32766)
 
         var_mean = data_bbox.mean()
@@ -286,7 +368,12 @@ def main():
         return var_mean.values, var_max.values, var_std.values
 
     logging.info('Reading forest information...')
-    for name, filename in paths:
+    pbar = ProgressBar()
+    pbar.register()
+
+    iterations = len(g_dataset) * len(paths)
+
+    for name, filename in tqdm(paths):
         #g_dataset['mean {}'.format(name)], g_dataset['median {}'.format(name)], g_dataset['max {}'.format(name)], g_dataset['std {}'.format(name)], g_dataset['9th decile {}'.format(name)], g_dataset['1st decile {}'.format(name)] = zip(*g_dataset.apply(lambda x: stats(x, ar), axis=1))
         g_dataset['mean {}'.format(name)], g_dataset['max {}'.format(name)], g_dataset['std {}'.format(name)] = zip(*g_dataset.apply(lambda x: stats(x, filename=filename), axis=1))
     logging.info('done')
@@ -323,7 +410,7 @@ def main():
     logging.info("dataset:\n{}".format(dataset.head(1)))
 
     # Save
-    save_dataset(dataset, db_params, table_name='classification_dataset_jse_forest')
+    save_dataset(dataset, db_params, table_name=options.dataset_table)
 
 
 
@@ -334,7 +421,8 @@ if __name__ =='__main__':
     parser.add_argument('--config_filename', type=str, default='cnf/rfc.ini', help='Config filename for training config')
     parser.add_argument('--config_name', type=str, default='test', help='Config section for training config')
     parser.add_argument('--db_config_filename', type=str, default='cnf/sasse_aws.yaml', help='CNF file containing DB connection pararemters')
-    #parser.add_argument('--db_config_name', type=str, default='production', help='Section name in db cnf file to read connection parameters')
+    parser.add_argument('--dataset', type=str, default='loiste_jse', help='Which dataaset to create (loiste_jse|energiateollisuus)')
+    parser.add_argument('--dataset_table', type=str, default='classification_dataset_jse_forest', help='DB tablename')
 
     if len(sys.argv) <= 1:
         parser.print_help()
@@ -347,6 +435,10 @@ if __name__ =='__main__':
                         level=logging.INFO)
 
     logging.info('Using config {} from {}'.format(options.config_name, options.config_filename))
+    logging.getLogger('boto3').setLevel(logging.WARNING)
+    logging.getLogger('botocore').setLevel(logging.CRITICAL)
+    logging.getLogger('s3transfer').setLevel(logging.CRITICAL)
+    logging.getLogger('urllib3').setLevel(logging.CRITICAL)
 
     conf_bucket  ='fmi-sasse-cloudformation'
     conf_file    = 'smartmet.yaml'
