ca04df53c6160deda92bdf9a752b71591c91670a
Cloud User <centos@ip-172-31-19-166.eu-west-1.compute.internal>
2020-04-15 06:24:36 +0000

    dos and donts

--- bin/process_tree_files.sh
+++ bin/process_tree_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# sudo mkdir -p /data && sudo chown 775 /data && sudo chcon -Rt svirt_sandbox_file_t /data
 folder=$1
 files=""
 for f in `aws s3 ls s3://fmi-asi-data-puusto/luke/2017/$folder/`;
@@ -8,9 +10,19 @@ do
   fi
 done
 
-cmd="docker run -v ${HOME}:/root -v /tmp:/tmp --rm osgeo/gdal gdal_merge.py -init -init 32767 -a_nodata 32767 -o /tmp/${folder}_suomi_3067.tif ${files}"
-echo $cmd && eval "$cmd"
-cmd="docker run -v ${HOME}:/root -v /tmp:/tmp --rm osgeo/gdal gdalwarp -t_srs EPSG:4326 /tmp/${folder}_suomi_3067.tif /tmp/${folder}_suomi_4326.tif"
-echo $cmd && eval "$cmd"
-cmd="aws s3 cp /tmp/${folder}_suomi_4326.tif s3://fmi-asi-data-puusto/luke/2017/${folder}/${folder}_suomi_4326.tif"
-echo $cmd && eval "$cmd"
+BASE="/data"
+FILE_3067="${BASE}/${folder}_suomi_3067.tif"
+FILE_4326="${BASE}/${folder}_suomi_4326.tif"
+
+cmd="docker run -v ${HOME}:/root -v /data:/data:rw --rm osgeo/gdal gdal_merge.py -init 32767 -a_nodata 32767 -o ${FILE_3067} ${files}"
+echo $cmd 
+$cmd
+cmd="docker run -v ${HOME}:/root -v /data:/data:rw --rm osgeo/gdal gdalwarp -t_srs EPSG:4326 ${FILE_3067} ${FILE_4326}"
+echo $cmd
+$cmd
+
+docker run -v $HOME:/root -v /data:/data:rw --rm osgeo/gdal gdalinfo -stat -hist $FILE_4326
+
+cmd="aws s3 cp ${FILE_4326} s3://fmi-asi-data-puusto/luke/2017/${folder}/${folder}_suomi_4326.tif"
+echo $cmd
+$cmd
